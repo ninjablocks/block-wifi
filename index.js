e972e6eb11fadcaf032065404adf836fff1ecb1f
Emily Rose <emily@contactvibe.com>
2012-09-18 02:24:25 -0700

    Added exception handler. Now forking monitor process for WiFi tasks.

--- index.js
+++ index.js
@@ -10,16 +10,66 @@
  * Emily Rose <emily@ninjablocks.com>
  */ 
 
-;(function() {
+ ;(function() {
 	
 	var
 		argv = require('optimist').argv
-		, route = require('./routes')
+		, config = require('./app/config')
+		, fork = require('child_process').fork
 		, express = require('express')
 		, port = argv.port || 80
+		, util = require('util')
 		, app = express()
+		, monitor
 	;
 
-	route(app).listen(port);
+	function exit(reason) {
+
+		process.stderr.write(
+
+			util.format(
+
+				"ERROR: Could not listen on port %s. %s\n"
+				, port
+				, reason
+			)
+		);
+		process.exit(1);
+	};
+
+	/**
+	 * Messages from the monitor proccess
+	 */
+	function message(msg) {
+
+		if(!msg.action || typeof msg.data == 'undefined' || msg.error) {
+
+			return;
+		}
+		console.log(msg);
+		app.emit(msg.action, msg.data);
+	};
+
+	/**
+	 * Exit if we have problems. Upstart should handle restarts.
+	 */
+	process.on("uncaughtException", function(err) {
+
+		if(err.code == "EACCES") {
+
+			exit("Access denied.");
+
+		}
+		else if(err.code == "EADDRINUSE") {
+
+			exit("Address in use.");
+		}
+	});
+
+	monitor = fork('./monitor');
+	monitor.on('message', message);
+	monitor.send({ action : "init" });
+
+	config(app).listen(port);
 
 })();
