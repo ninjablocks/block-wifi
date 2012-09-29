7b4b54fad39f856e1087cdc42aeeca810ad1c1a0
Emily Rose <emily@contactvibe.com>
2012-09-29 01:29:22 -0700

    Added monitor intervals for deviceCheck, ifaceCheck, wifiScan.

--- monitor.js
+++ monitor.js
@@ -3,16 +3,25 @@ var
 	, opts = { timout : 10000 }
 	, wifiScan = require('./bin/wifi_scan')
 	, deviceCheck = require('./bin/device_check')
+	, ifaceCheck = require('./bin/iface_check')
 	, writeConfig = require('./bin/write_config')
+	, resetWifi = require('./bin/reset_wifi')
 	, actions = {
 
 		"wifiScan" : wifiScan
 		, "deviceCheck" : deviceCheck
 		, "writeConfig" : writeConfig
+		, "ifaceCheck" : ifaceCheck
+		, "resetWifi" : resetWifi
 		, "init" : function() {
 
-			handleMessage({ action : "wifiScan" });
-			handleMessage({ action : "deviceCheck" });
+			deviceCheck();
+			ifaceCheck();
+			wifiScan();
+
+			setInterval(deviceCheck, 10000);
+			setInterval(ifaceCheck, 10000);
+			setInterval(wifiScan, 10000);
 		}
 	}
 ;
@@ -22,9 +31,9 @@ function handleMessage(dat) {
 	if(!dat) { return; }
 	
 	var action = dat.action || undefined;
-	if((actions[action]) && typeof (actions[action] == "function")) {
+	if((actions[action]) && typeof actions[action] == "function") {
 
-		actions[action]();
+		actions[action](dat.data || null);
 	}	
 };
 
