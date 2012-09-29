56ddf61eb262ec42fe5cb6b5a446df7bded1e86b
Emily Rose <emily@contactvibe.com>
2012-09-29 01:26:47 -0700

    Reset wifi and write config.

--- app/routes/post-connect.js
+++ app/routes/post-connect.js
@@ -1,20 +1,43 @@
 ;(function() {
+
+	var cells = [];
 	
 	module.exports = function(app) {
 
-		app.post('/connect', function(req, res, next) {
+		app.on('wifiScan', function(dat) {
+			
+			if((typeof dat !== "object") || dat.length < 1) { return; }
 
-			var 
-				basestation = req.params.basestation.split(',')
-				, password = req.params.password
+			dat.forEach(function(cell) {
+
+				cells[cell.ssid] = cell;
+			});
+		});
+
+		app.post('/connect', function(req, res, next) {
+			
+			var
+				conf = {
+
+					'ssid' : req.body.ssid
+					, 'password' : req.body.password
+				}
 			;
 
-			// parse basestation information
+			if(!cells[conf.ssid]) {
 
-			// cycle wlan0
-			// write-out supplicant
+				return res.end("unknown network.");
+			}
 
-			res.end("ohai");
+			cells[conf.ssid].password = conf.password;
+
+			// request wlan0 cycle
+			app.send('resetWifi', true);
+
+			// request config write-out
+			app.send("writeConfig", cells[conf.ssid]);
+
+			res.render('connected');
 		});
 	};
 
