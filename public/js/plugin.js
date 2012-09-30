723983424430da49c61f603ebaa733344e8c74cb
Emily Rose <emily@contactvibe.com>
2012-09-30 16:24:57 -0700

    Added jQuery. Added plugin.js with basic functionality to probe for device connection and reload page.

---
+++ public/js/plugin.js
@@ -0,0 +1,43 @@
+;(function() {
+	
+	var 
+		check = function() {
+
+			$.ajax({
+
+				type : "GET"
+				, url : "/device"
+				, dataType : "JSON"
+				, success : connected
+				, failure : error
+			})
+		}
+		, connected = function(dat) {
+
+			if((dat)) {
+
+				if(dat.device) {
+
+					/**
+					 * Reload and let the app reroute
+					 * us appropriately.
+					 */
+					window.location.href = '/';
+				}
+				else {
+
+					/**
+					 * Wait for them to plug it in
+					 */
+					setTimeout(check, 1000);
+				}
+			}
+		}
+		, error = function() {
+
+		}
+	;
+
+	$(check);
+
+})();
