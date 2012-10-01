4d284e560f7fadba9b2d5d05e34f8a1853500e2b
Emily Rose <emily@contactvibe.com>
2012-10-01 07:14:16 -0700

    Added much functionality. Retrieving networks, posting connect request.

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,56 @@
 ;(function() {
 	
+	var cells = {};
+
+	var init = function() {
+
+		$('#rescan').click(function() { window.location.href = '/'; });
+		$('#connect').click(connect);
+		$('.alert, div.control-group.hidden').hide().removeClass('hidden');
+		$('select').on('change', choice);
+
+		$('#password').keyup(function(e) {
+
+			e.preventDefault();
+
+			if($(this).val().length > 0) {
+
+				$('#connect').removeClass('disabled').removeAttr('disabled');
+			}
+			else {
+				$('#connect').addClass('disabled').attr('disabled', 'disabled');
+			}
+		});
+		// scanning for wifi alert
+		$($('.alert-info')[0]).fadeIn();
+
+		networks();
+	};
+
+	var choice = function(e) {
+
+		e.preventDefault();
+		var val = $(this).val();
+		console.log(val);
+		console.log(cells);
+		if(val !== "null" && cells[val]) {
+
+			if(cells[val].encryption == true) {
+
+				return $($('div.control-group')[1]).slideDown();
+			}
+		}
+		$($('div.control-group')[1]).slideUp();
+	};
+
+	var connect = function(e) {
+
+		e.preventDefault();
+		var dat = $('form').serializeObject();
+
+		console.log(dat);
+	};
+
 	var networks = function() {
 
 		$.ajax({
@@ -7,7 +58,7 @@
 			type : 'GET'
 			, url : '/networks'
 			, dataType : 'JSON'
-			, success : networkList
+			, success : function(dat) { setTimeout(function() { networkList(dat) }, 2000); }
 			, failure : networks
 		})
 	};
@@ -24,8 +75,7 @@
 
 	var networkList = function(dat) {
 
-		console.log(dat);
-
+		$('.alert:visible').slideUp();
 		if((dat) && (dat.networks)) {
 
 			var 
@@ -33,17 +83,56 @@
 				, select = $('#networks')
 			;
 
-			list.forEach(function(option) {
+			// save each network in cell list
+			dat.networks.forEach(function(cell) { cells[cell.address] = cell; });
 
-				select.append(option);
-			})
+			select.html('');
+
+			if(dat.networks.length > 0) {
+
+				list.forEach(function(option) {
+
+					select.append(option);
+				});
+
+				select.prepend($('<option>').attr('value', 'null').html('&nbsp;'));
+				$($('.control-group')[0]).slideDown();
+			}
+			else {
+
+				// no networks found.
+				$($('.alert-info')[1]).fadeIn();		
+			}
 		}
 		else {
 
 			// some error happened.
+			$($('.alert-info')[2]).fadeIn();					
 		}
 	};
 
-	$(networks);	
+	$(init);	
+
+	$.fn.serializeObject = function(){
+
+		var o = {};
+		var a = this.serializeArray();
+		$.each(a, function() {
+
+			if (o[this.name]) {
+
+				if (!o[this.name].push) {
+
+					o[this.name] = [o[this.name]];
+				}
+				o[this.name].push(this.value || '');
+			} 
+			else{
+
+				o[this.name] = this.value || '';
+			}
+		});
+		return o;
+	};
 
 })();
