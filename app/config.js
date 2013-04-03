38765d251ec92581695e482609bd6dd93880052d
Emily Rose <emily@contactvibe.com>
2013-04-03 11:32:54 -0700

    Added cookieParser and cookieSession to express config. Added this context to middleware.

--- app/config.js
+++ app/config.js
@@ -3,23 +3,29 @@ var
 	, express = require('express')
 	, route = require('./routes')
 	, events = require('events')
+	, crypto = require('crypto')
 	, util = require('util')
 	, path = require('path')
 ;
 
 module.exports = function(app) {
 	
-	var mids = middleware(app);	
+	var 
+		mids = middleware.call(this, app)
+		, rand = crypto.randomBytes(7).toString('base64')
+	;	
 	
 	events.EventEmitter.call(app);
 	util.inherits(events.EventEmitter, app);
 
 	app.set('view engine', 'ejs');
 	app.set('views', path.resolve(__dirname, '..', 'views'));
-	app.use(express.static(path.resolve(__dirname, '..', 'public')));	
+	app.use(express.static(path.resolve(__dirname, '..', 'public')));
 	app.use(express.methodOverride());
 	app.use(express.bodyParser());
 	app.use(express.favicon());
+	app.use(express.cookieParser(rand));
+	app.use(express.cookieSession());	
 	
 	/**
 	 * We can log everything here
